87234ef92259b1adacf4a111c89847a5a4ce6091
placebetsai <placebetsai@gmail.com>
2025-12-05 15:52:29 -0500

    Create rank-your-school.js

---
+++ pages/rank-your-school.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+export default function RankYourSchool() {
+  return (
+    <Layout title="Rank Your School">
+      <h1>Rank Your School Placeholder</h1>
+    </Layout>
+  );
+}
