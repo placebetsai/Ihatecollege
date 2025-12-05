be7c4e00190374566304e927270ac0f174541898
placebetsai <placebetsai@gmail.com>
2025-12-05 15:51:51 -0500

    Create debt-calculator.js

---
+++ pages/debt-calculator.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+export default function DebtCalc() {
+  return (
+    <Layout title="Debt Calculator">
+      <h1>Debt Calculator Placeholder</h1>
+    </Layout>
+  );
+}
