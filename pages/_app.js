8f290379c33b6e6864b1782e8051c246f09c1b42
placebetsai <placebetsai@gmail.com>
2025-12-05 16:10:11 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
 import "../styles/globals.css";
-import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <div className="app-bg min-h-screen">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </div>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;
