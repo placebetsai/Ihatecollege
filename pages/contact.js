f8cc3b834d1c1dc0793354ea24506c24dcdbf82b
placebetsai <placebetsai@gmail.com>
2025-12-05 15:54:22 -0500

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+export default function Contact() {
+  return (
+    <Layout title="Contact">
+      <h1>Contact Placeholder</h1>
+    </Layout>
+  );
+}
