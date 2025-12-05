1377ecf6bfb5a98ff3e0b9d4012a60889cc9c454
placebetsai <placebetsai@gmail.com>
2025-12-05 15:52:50 -0500

    Create cheat-sheets.js

---
+++ pages/cheat-sheets.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+export default function CheatSheets() {
+  return (
+    <Layout title="Cheat Sheets">
+      <h1>Cheat Sheets Placeholder</h1>
+    </Layout>
+  );
+}
