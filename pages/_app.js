f9ba368785abc3e9c06d0aa945ae1b08d691943c
placebetsai <placebetsai@gmail.com>
2025-12-06 06:44:37 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,6 @@
-
 // pages/_app.js
 import "../styles/globals.css";
-import Layout from "../components/Layout";
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
