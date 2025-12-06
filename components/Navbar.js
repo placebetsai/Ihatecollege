f36b3e6ef56713c0850ee27719ca004bdd75c873
placebetsai <placebetsai@gmail.com>
2025-12-05 20:47:00 -0500

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,13 +5,16 @@ export default function Navbar() {
   return (
     <header className="site-header">
       <div className="site-header-inner">
+        {/* LOGO */}
         <Link href="/" className="site-logo">
-          IHATECOLLEGE<span>.COM</span>
+          <span className="logo-main">IHATECOLLEGE</span>
+          <span className="logo-tag">.COM</span>
         </Link>
 
+        {/* NAV LINKS */}
         <nav className="site-nav">
           <Link href="/">Home</Link>
-          <Link href="/alternatives">Alternatives</Link>
+          <Link href="/Alternatives">Alternatives</Link>
           <Link href="/debt-calculator">Debt Calculator</Link>
           <Link href="/cheat-sheets">Cheat Sheets</Link>
           <Link href="/rank-your-school">Rank Your School</Link>
