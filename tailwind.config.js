c5f805dd6cdf583c5c7ac9b3422a3e618ea0b8e7
public/og-card.png <placebetsai@gmail.com>
2025-12-11 10:43:49 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          950: "#0a0a0a",
+        },
+      },
+    },
   },
   plugins: [],
 }
